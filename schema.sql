60c6b1ce6e93222068c421e4315953958cb333f3
Calvin Turner <turnerjrcalvin@gmail.com>
2022-03-29 23:37:22 -0400

    create db

--- schema.sql
+++ schema.sql
@@ -10,5 +10,18 @@ CREATE TABLE department(
 );
 
 CREATE TABLE role (
-    id INT NOT NULL
-)
+    id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NUll,
+    department_id INT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY(id)
+);
